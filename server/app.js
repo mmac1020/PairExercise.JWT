d9a6e2dd6df8cbba8dd0bb5b627009215c6a4583
Michael McInerney <michael.mcinerney@fullstackacademy.com>
2021-04-02 10:44:01 -0400

    Adding code from workshop

---
+++ server/app.js
@@ -0,0 +1,34 @@
+const express = require('express');
+const app = express();
+const jwt = require('jsonwebtoken');
+app.use(express.json());
+const {
+  models: { User },
+} = require('./db');
+const path = require('path');
+
+app.get('/', (req, res) => res.sendFile(path.join(__dirname, '..', 'public', 'index.html')));
+
+app.post('/api/auth', async (req, res, next) => {
+  try {
+    res.send({ token: await User.authenticate(req.body) });
+  } catch (ex) {
+    next(ex);
+  }
+});
+
+app.get('/api/auth', async (req, res, next) => {
+  try {
+    const legitToken = jwt.verify(req.headers.authorization, process.env.JWT)
+    res.send(await User.byToken(legitToken));
+  } catch (ex) {
+    next(ex);
+  }
+});
+
+app.use((err, req, res, next) => {
+  console.log(err);
+  res.status(err.status || 500).send({ error: err.message });
+});
+
+module.exports = app;
